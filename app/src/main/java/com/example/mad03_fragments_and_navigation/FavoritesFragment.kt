3629d08f307d6654ebd45ff8587cb969fd586937
c1910475048 <julia.frings@stud.fh-campuswien.ac.at>
2021-05-22 15:27:13 +0200

    Dialog not working yet

--- app/src/main/java/com/example/mad03_fragments_and_navigation/FavoritesFragment.kt
+++ app/src/main/java/com/example/mad03_fragments_and_navigation/FavoritesFragment.kt
@@ -24,11 +24,13 @@ import com.example.mad03_fragments_and_navigation.models.Movie
 import com.example.mad03_fragments_and_navigation.repositories.MovieRepository
 import com.example.mad03_fragments_and_navigation.viewmodels.MovieFavoritesViewModel
 import com.example.mad03_fragments_and_navigation.viewmodels.MovieFavoritesViewModelFactory
+import kotlinx.android.synthetic.main.dialog_edit_movie.*
 
 
 class FavoritesFragment : Fragment() {
     private lateinit var binding: FragmentFavoritesBinding
-    //private lateinit var bindingDialog: DialogEditMovieBinding
+    private lateinit var bindingDialog: DialogEditMovieBinding
+
     private lateinit var sharedViewModel: MovieFavoritesViewModel
     private lateinit var dao: MovieDao
     private lateinit var repository: MovieRepository
@@ -57,11 +59,11 @@ class FavoritesFragment : Fragment() {
 
         val adapter = FavoritesListAdapter(
             dataSet = listOf(),     // start with empty list
-            onDeleteClicked = {movieId -> onDeleteMovieClicked(movieId)},   // pass functions to adapter
-            onEditClicked = {movie -> onEditMovieClicked(movie)},           // pass functions to adapter
+            onDeleteClicked = { movieId -> onDeleteMovieClicked(movieId) },   // pass functions to adapter
+            onEditClicked = { movie -> onEditMovieClicked(movie) },           // pass functions to adapter
         )
 
-        with(binding){
+        with(binding) {
             recyclerView.adapter = adapter
         }
 
@@ -71,52 +73,76 @@ class FavoritesFragment : Fragment() {
     }
 
     // This is called when recyclerview item edit button is clicked
-    private fun onEditMovieClicked(movieObj: Movie){
+    private fun onEditMovieClicked(movieObj: Movie) {
         // TODO implement me
 
-        showNoteDialog(movieObj)
+        showDialog(movieObj)
 
         //sharedViewModel.editMovie(movieObj)
         Log.i("FavoritesFragment", "onEditMovieClicked: $movieObj")
     }
 
     // This is called when recyclerview item remove button is clicked
-    private fun onDeleteMovieClicked(movieId: Long){
+    private fun onDeleteMovieClicked(movieId: Long) {
         // TODO implement me
         sharedViewModel.deleteMovie(movieId)
         Log.i("FavoritesFragment", "onDeleteMovieClicked: $movieId")
     }
 
-    private fun onClearClicked(){
+    private fun onClearClicked() {
 
     }
 
-    private fun subscribeUI(adapter: FavoritesListAdapter){
+    private fun subscribeUI(adapter: FavoritesListAdapter) {
 
-        sharedViewModel.favoriteMovies.observe(viewLifecycleOwner, Observer { favoriteMovies -> adapter.updateDataSet(favoriteMovies) })
+        sharedViewModel.favoriteMovies.observe(
+            viewLifecycleOwner,
+            Observer { favoriteMovies -> adapter.updateDataSet(favoriteMovies) })
 
     }
 
     fun showNoteDialog(movieObj: Movie) {
-        // Create an instance of the dialog fragment and show it
-        val dialog = NoticeDialogFragment(movieObj)
-        dialog.show(childFragmentManager, "NoticeDialogFragment")
+
     }
 
-    /*
-    // The dialog fragment receives a reference to this Activity through the
-    // Fragment.onAttach() callback, which it uses to call the following methods
-    // defined by the NoticeDialogFragment.NoticeDialogListener interface
-    override fun onDialogPositiveClick(dialog: DialogFragment) {
-        // User touched the dialog's positive button
-        Log.i("FavoritesFragment","Save pressed")
+    fun showDialog(movieObj: Movie) {
+
+        val builder = AlertDialog.Builder(requireContext())
+        // Get the layout inflater
+        val inflater = requireActivity().layoutInflater;
+        val dialogLayout = DialogEditMovieBinding.inflate(layoutInflater)
+
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(dialogLayout.root)
+            // Add action buttons
+            .setPositiveButton(R.string.save) { _, _ ->
+                var newNote: String = editText_add_a_note.text.toString()
+                Log.i("FavoritesFragment","newNote: $newNote")
+                movieObj.note = newNote
+            }
+            .setNegativeButton(R.string.cancel) { _, _ ->
+
+            }
+        builder.create().show()
     }
 
-    override fun onDialogNegativeClick(dialog: DialogFragment) {
-        // User touched the dialog's negative button
-        Log.i("FavoritesFragment","Cancel pressed")
-    }
-     */
+
+/*
+// The dialog fragment receives a reference to this Activity through the
+// Fragment.onAttach() callback, which it uses to call the following methods
+// defined by the NoticeDialogFragment.NoticeDialogListener interface
+override fun onDialogPositiveClick(dialog: DialogFragment) {
+    // User touched the dialog's positive button
+    Log.i("FavoritesFragment","Save pressed")
+}
+
+override fun onDialogNegativeClick(dialog: DialogFragment) {
+    // User touched the dialog's negative button
+    Log.i("FavoritesFragment","Cancel pressed")
+}
+ */
 
 
 }
